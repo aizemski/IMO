c8634ae6364551dc8f1527bb1cc2248c7e282360
aizemski <aleksanderizemski@gmail.com>
2021-03-03 23:33:35 +0100

    Cleaning

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -3,6 +3,7 @@ import random
 import matplotlib.pyplot as plt
 
 
+
 def readTSP(filename):
     nodelist = []
     with open(filename,'r') as file:
@@ -24,44 +25,50 @@ def readTSP(filename):
         # Close input file
 
     return nodelist
-
+nodes = readTSP("kroA100.tsp")
 def count_dist(v, u):
 
     return np.sqrt(((v[0] - u[0]) ** 2)+((v[1] - u[1]) ** 2))
 
 def create_dst_matrix(nodes):
+    dst_matrix_sorted = []
     dst_matrix = []
     for i in range(len(nodes)):
+        dst_matrix_sorted.append([])
         dst_matrix.append([])
         for j in range(len(nodes)):
-            dst_matrix[i].append([count_dist(nodes[i],nodes[j]),j])
+            dst_matrix_sorted[i].append([count_dist(nodes[i],nodes[j]),j])
         #nearest 
-        dst_matrix[i].sort()
-    return dst_matrix
+        dst_matrix[i].append(dst_matrix_sorted[i])
+        dst_matrix_sorted[i].sort()
+    return dst_matrix_sorted ,dst_matrix
 
 def min_index_value(indexes , node_neighbors):
     index = 0
     while node_neighbors[index][1] in indexes:
         index +=1
-    return node_neighbors[index][1] , node_neighbors[index]
+    return node_neighbors[index][1] , node_neighbors[index][0]
+
 def nearest_neighbour_execute(cycle,size_of_cycle,dst_matrix,second_cycle):
     while len(cycle) < size_of_cycle:
     
         # check on which end is closer node
-        index, index_value = min_index_value(cycle+second_cycle,dst_matrix[cycle[0]])
-        last_index, last_index_value =  min_index_value(cycle+second_cycle,dst_matrix[cycle[-1]])
+        index_list =[]
+        index_value_list=[]
+        for i in cycle:
+            index, index_value = min_index_value(cycle+second_cycle,dst_matrix[i])
+            index_list.append(index)
+            index_value_list.append(index_value)
+        min_val = min(index_value_list)
         
-        # add node at beginnig
-        if index_value > last_index_value:
-            cycle.insert(0,index)
-        # add node at end
-        else:
-            cycle.append(last_index)
+        for i in range(len(index_value_list)):
+            if index_value_list[i] == min_val:
+                cycle.insert(i+1,index_list[i])  
+                break
     return cycle
 
 def nearest_neighbour(dst_matrix):
     # start from first node
-    
     length =  len(dst_matrix[0])
     size_of_cycle = length//2
     first_cycle = [0]#[random.randint(0, length-1)]
@@ -72,7 +79,6 @@ def nearest_neighbour(dst_matrix):
     while second_cycle[0] in first_cycle:
         second_cycle[0]+=1
     size_of_second_cycle = length - size_of_cycle
-    
     second_cycle = nearest_neighbour_execute(second_cycle,size_of_second_cycle,dst_matrix,first_cycle)
 
     return [first_cycle , second_cycle]
@@ -96,7 +102,8 @@ def best_new(cycle,first,second,first_neighbours,second_neighbours):
 def lowest_cost(nearest,cycle_dst):
     # print(nearest)
     # print(cycle_dst)
-    # print()
+    # input()
+
     cost = nearest[0][1] - cycle_dst[0]
 
     index=0
@@ -106,6 +113,17 @@ def lowest_cost(nearest,cycle_dst):
             cost = nearest[i][1] - cycle_dst[i]
     return index
 
+def cycle_expansion_execute(cycle,cycle_dst,size_of_cycle,dst_matrix,second_cycle):
+    while len(cycle) < size_of_cycle:
+        nearest = []
+        for i in range(len(cycle)-1):
+            nearest.append(best_new(second_cycle+cycle,cycle[i],cycle[i+1],dst_matrix[cycle[i]],dst_matrix[cycle[i+1]]))
+        #find lowest cost
+        index = lowest_cost(nearest,cycle_dst)
+
+        cycle.insert(index+1,nearest[index][0])
+        cycle_dst.insert(index+1,nearest[index][1])
+    return cycle
 def cycle_expansion(dst_matrix):
     length =  len(dst_matrix[0])
     size_of_cycle = length//2
@@ -114,17 +132,9 @@ def cycle_expansion(dst_matrix):
     # first node is the nearest one
     first_index, first_index_value = min_index_value(first_cycle,dst_matrix[first_cycle[0]])
     first_cycle.append(first_index)
-    first_cycle_dst.append(first_index_value[0])
+    first_cycle_dst.append(first_index_value)
+    first_cycle = cycle_expansion_execute(first_cycle,first_cycle_dst,size_of_cycle,dst_matrix,[])
 
-    while len(first_cycle) < size_of_cycle:
-        nearest = []
-        for i in range(len(first_cycle)-1):
-            nearest.append(best_new(first_cycle,first_cycle[i],first_cycle[i+1],dst_matrix[first_cycle[i]],dst_matrix[first_cycle[i+1]]))
-        #find lowest cost
-        index = lowest_cost(nearest,first_cycle_dst)
-
-        first_cycle.insert(index,nearest[index][0])
-        first_cycle_dst.insert(index,nearest[index][1])
     second_cycle = [0]
 
     # looking for first node outside of first cycle
@@ -135,17 +145,8 @@ def cycle_expansion(dst_matrix):
     # second node is the nearest one
     second_index, second_index_value = min_index_value(second_cycle,dst_matrix[second_cycle[0]])
     second_cycle.append(second_index)
-    second_cycle_dst.append(second_index_value[0])
-    while len(second_cycle) < size_of_second_cycle:
-        nearest = []
-        for i in range(len(second_cycle)-1):
-            nearest.append(best_new(first_cycle+second_cycle,second_cycle[i],second_cycle[i+1],dst_matrix[second_cycle[i]],dst_matrix[second_cycle[i+1]]))
-        #find lowest cost
-        index = lowest_cost(nearest,second_cycle_dst)
-
-        second_cycle.insert(index,nearest[index][0])
-        second_cycle_dst.insert(index,nearest[index][1])
-
+    second_cycle_dst.append(second_index_value)
+    second_cycle = cycle_expansion_execute(second_cycle,second_cycle_dst,size_of_second_cycle,dst_matrix,first_cycle)
     return [first_cycle, second_cycle]
 
 def regret_2():
@@ -172,8 +173,10 @@ def display(nodes, indexes):
     draw_lines(nodes,indexes[1])
     plt.show()
     # pass
-nodes = readTSP("kroA100.tsp")
-dst_matrix = create_dst_matrix(nodes)
-indexes = nearest_neighbour(dst_matrix)
-# indexes = cycle_expansion(dst_matrix)
-display(nodes, indexes)
+
+dst_matrix_sorted, dst_matrix = create_dst_matrix(nodes)
+
+for i in range(10):
+    # indexes = nearest_neighbour(dst_matrix_sorted)
+    indexes = cycle_expansion(dst_matrix_sorted)
+    display(nodes, indexes)
